853055c12a85fd8e56d324ab4742105b9fd33b6e
caseywatts <casey.s.watts@gmail.com>
2012-06-21 17:32:51 -0400

    removed more indexes that pg hates

--- db/schema.rb
+++ db/schema.rb
@@ -120,9 +120,6 @@ ActiveRecord::Schema.define(:version => 20120619213311) do
     t.datetime "updated_at", :null => false
   end
 
-  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
-  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
-
   create_table "settings", :force => true do |t|
     t.string   "var",                      :null => false
     t.text     "value"
