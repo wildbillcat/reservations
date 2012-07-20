06bc8a357241cbc18b1ed2f4b21a84ad031bcf55
Ian Suvak <ian.suvak@yale.edu>
2012-07-20 17:26:06 -0400

    another merge conflict that escaped me

--- app/models/app_config.rb
+++ app/models/app_config.rb
@@ -1,38 +1,13 @@
 class AppConfig < ActiveRecord::Base
 
-<<<<<<< HEAD
-  attr_accessible 	:site_title,
-          :admin_email,
-					:department_name,
-          :contact_link_location,
-					:home_link_text,
-					:home_link_location,
-          :upcoming_checkin_email_body,
-					:upcoming_checkin_email_active,
-					:overdue_checkout_email_body,
-					:overdue_checkout_email_active,
-					:overdue_checkin_email_body,
-					:overdue_checkin_email_active,
-					:reservation_confirmation_email_active,
-					:default_per_cat_page,
-					:terms_of_service,
-          :auth_provider,
-          :ldap_host,
-          :ldap_first_name,
-          :ldap_last_name,
-          :ldap_port,
-          :ldap_base_query,
-          :ldap_email,
-          :ldap_login,
-          :ldap_phone
-  
-=======
   attr_accessible :site_title, :admin_email, :department_name,:contact_link_location,
                   :home_link_text, :home_link_location, :upcoming_checkin_email_body,
                   :upcoming_checkin_email_active, :overdue_checkout_email_body,
                   :overdue_checkout_email_active, :overdue_checkin_email_body,
                   :overdue_checkin_email_active, :reservation_confirmation_email_active,
-                  :default_per_cat_page, :terms_of_service, :favicon
+                  :default_per_cat_page, :terms_of_service, :favicon, :auth_provider,
+                  :ldap_host, :ldap_first_name, :ldap_last_name, :ldap_port,
+                  :ldap_phone, :ldap_login, :ldap_email
 
   has_attached_file :favicon, url: "/system/:attachment/:id/:style/favicon.:extension"
  
@@ -40,7 +15,6 @@ class AppConfig < ActiveRecord::Base
                                       message: "Must be .ico"
 
 
->>>>>>> development
   validates :site_title, 	:presence => true,
 							:length => {:maximum => 20 }
   validates :admin_email,
