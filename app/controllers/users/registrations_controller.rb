aeab9f3f0ce6cfa3872c914dde0dcca46df7602a
Ian Suvak <ian.suvak@yale.edu>
2012-07-22 13:42:38 -0400

    fixed update action

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -28,9 +28,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
       return
     end
 
-   # The code below is content of Devise Registrations#create method except for build_resource because then
-  # it rebuilds it and forgets about setting the login to be e-mail so the validation fails 
-    
+    # The code below is content of Devise Registrations#create method except for build_resource because then
+    # it rebuilds it and forgets about setting the login to be e-mail so the validation fails 
+
     if resource.save
       if resource.active_for_authentication?
         set_flash_message :notice, :signed_up if is_navigational_format?
@@ -46,4 +46,27 @@ class Users::RegistrationsController < Devise::RegistrationsController
       respond_with resource
     end
   end
+
+  def update
+    self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+
+    binding.pry
+    resource.login = params[:user][:email]
+    binding.pry
+
+    if resource.update_with_password(resource_params)
+      if is_navigational_format?
+        if resource.respond_to?(:pending_reconfirmation?) && resource.pending_reconfirmation?
+          flash_key = :update_needs_confirmation
+        end
+        set_flash_message :notice, flash_key || :updated
+      end
+      sign_in resource_name, resource, :bypass => true
+      respond_with resource, :location => after_update_path_for(resource)
+    else
+      clean_up_passwords resource
+      respond_with resource
+    end
+  end
+
 end
